0da98dda26810c866c51a26f6dba47eb5c4b6e3b
Jordan Marcus <jormarcus92@gmail.com>
2018-06-12 16:59:02 -0400

    first push

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
 const express = require("express");
+const morgan = require('morgan');
+const postBank = require('./postBank');
+
 const app = express();
+app.use(morgan('dev'));
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get('/', (req, res) => {
+  //first get the list of posts
+  const posts = postBank.list();
+
+  //then prepare some html to send as output
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+  </head>
+  <body>
+    <ul>
+    ${posts.map(post => '<li>${}</li>')}
+    </ul>
+  </body>
+  </html>`;
+
+  //finally send a response
+  res.send(html);
+});
+
+
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
+
